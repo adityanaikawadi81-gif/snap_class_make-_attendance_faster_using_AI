38f4a43a54b31b29a6c34cd8ecd10483b38ee45d
Aditya Naikawadi <adityanaikawadi601@gmail.com>
2026-08-12 12:38:13 +0530

    Fix heading color

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -56,6 +56,7 @@ def style_base_layout():
                 font-size:3.5rem !important;
                 line-height: 1.1 !important;
                 margin-bottom:0rem !important;
+                color: #2B2B2B !important;
             }
                 
 
@@ -64,10 +65,12 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height: 0.9 !important;
                 margin-bottom:0rem !important;
+                color: #2B2B2B !important;
             }
                 
             h3, h4, p {
-                font-family: 'Outfit', sans-serif;    
+                font-family: 'Outfit', sans-serif !important; 
+                color: #2B2B2B !important;   
             }
                 
 
