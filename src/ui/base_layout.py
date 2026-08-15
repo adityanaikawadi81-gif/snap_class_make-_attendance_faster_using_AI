75d59a900435c1ace6635fc3fbe57a5f2f291621
Aditya Naikawadi <adityanaikawadi601@gmail.com>
2026-08-15 17:49:20 +0530

    color change

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -75,7 +75,8 @@ def style_base_layout():
             }
                 
             h3, h4, p {
-                font-family: 'Outfit', sans-serif;   
+                font-family: 'Outfit', sans-serif;
+                color:black !important;   
             }
             
             div[data-testid="stTextInput"] label {
